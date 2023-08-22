25ea4ff089853b59a67db6f947c254731fbfe541
Radik <kalimullinrr@hotmail.com>
2023-08-22 18:26:20 +0400

    newBranchJunit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainAmount900() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemainAmount1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemainAmount999() {
+        int amount = 999;
+        int expected = 1;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemainAmount1001() {
+        int amount = 1001;
+        int expected = 999;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+}
